5464fbb3de2dccb05031a0519df51775030340d6
TC Wang <tingchun0113@gmail.com>
2020-08-26 21:00:48 +0800

    feat: add password_generator v1.0.0

---
+++ app.js
@@ -0,0 +1,29 @@
+// Include packages and define server related variables
+const express = require("express")
+const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
+const generatePassword = require("./generate_password")
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting routes
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.post("/", (req, res) => {
+  const options = req.body
+  const password = generatePassword(options)
+  res.render("index", { password, options })
+})
+
+app.listen(port, () => {
+  console.log(`The Express server is running on http://localhost:${port}`)
+})
